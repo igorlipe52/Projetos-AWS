32bb0f7f96aefe6404f3eb5921f952c0f378e2b0
Igor Barroso <140567532+igorlipe52@users.noreply.github.com>
2026-03-21 16:05:57 -0300

    Add main.tf for AWS instance configuration

---
+++ Terraform-Project-AWS/Instance/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+   
+} 
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+
+  config = {
+    bucket = "igor-lab-2026-terraform-state"
+    key    = "networking/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
+provider "aws" {
+  region = var.assume_role.region
+
+  default_tags {
+    tags = var.tags
+  }
+
+  assume_role {
+    role_arn = var.assume_role.arn
+  }
+}
