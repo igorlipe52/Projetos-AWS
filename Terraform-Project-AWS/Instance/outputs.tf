2525f65711c6beac882d5d5789427a7efc11c4ab
Igor Barroso <140567532+igorlipe52@users.noreply.github.com>
2026-03-21 16:08:22 -0300

    Refactor outputs.tf to remove unused output
    Removed commented-out public_dns output and cleaned up formatting.

--- Terraform-Project-AWS/Instance/outputs.tf
+++ Terraform-Project-AWS/Instance/outputs.tf
@@ -8,7 +8,3 @@ output "public_ip" {
   value       = aws_instance.ec2_instance.public_ip
 }
 
-/*output "public_dns" {
-  description = "DNS público da instância"
-  value       = aws_instance.ec2_instance.public_dns
-} */
